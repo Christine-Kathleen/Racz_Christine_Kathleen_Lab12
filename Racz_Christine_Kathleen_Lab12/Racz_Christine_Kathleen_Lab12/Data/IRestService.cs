5eb128bdf7d15cd3ec7e34726b6deeed3f4f9252
CHRISTINE-KATHLEEN RÁCZ <christine.racz@stud.ubbcluj.ro>
2021-01-06 15:26:57 +0200

    Add project files.

---
+++ Racz_Christine_Kathleen_Lab12/Racz_Christine_Kathleen_Lab12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Racz_Christine_Kathleen_Lab12.Models;
+
+namespace Racz_Christine_Kathleen_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
